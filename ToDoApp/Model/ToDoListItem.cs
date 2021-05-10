721cad0156ea99854390a37be7363779d3a2e825
Sam Hawkes <40278032+samhawkes@users.noreply.github.com>
2021-05-10 22:18:59 +0100

    Add model, view model and some basic xaml

---
+++ ToDoApp/Model/ToDoListItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+using ToDoApp.Helpers;
+
+namespace ToDoApp.Model
+{
+    public class ToDoListItem
+    {
+        public string ListItemText { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsSelected { get; set; }
+        public Guid ItemIdentifier { get; }
+        
+        public ICommand DeleteItemCommand { get; }
+        
+        public event EventHandler? DeleteItem ;
+        
+        public ToDoListItem(string listItemText)
+        {
+            ListItemText = listItemText;
+            IsActive = true;
+            IsSelected = false;
+            ItemIdentifier = Guid.NewGuid();
+
+            DeleteItemCommand = new SimpleCommand(_ => RaiseDeleteItem());
+        }
+
+        private void RaiseDeleteItem()
+        {
+            DeleteItem?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- ToDoApp/ViewModel/ToDoListViewModel.cs
+++ ToDoApp/ViewModel/ToDoListViewModel.cs
@@ -1,7 +1,68 @@
-﻿namespace ToDoApp.ViewModel
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Helpers;
+using ToDoApp.Model;
+
+namespace ToDoApp.ViewModel
 {
-    public class ToDoListViewModel
+    public class ToDoListViewModel : ObservableObject
     {
-        
+        private IEnumerable<ToDoListItem> _toDoListItems;
+        public ToDoListViewModel()
+        {
+            _toDoListItems = new List<ToDoListItem>();
+
+            var startingItems = new List<ToDoListItem>
+            {
+                new ToDoListItem("pee pee"),
+                new ToDoListItem("poo poo"),
+                new ToDoListItem("cah cah"),
+            };
+            UpdateListItems(startingItems);
+        }
+
+        public IEnumerable<ToDoListItem> ToDoItems
+        {
+            get => _toDoListItems;
+            set
+            {
+                if (_toDoListItems.Equals(value))
+                    return;
+
+                foreach (var item in _toDoListItems)
+                {
+                    item.DeleteItem -= ToDoItemOnDeleteItem;
+                }
+                
+                _toDoListItems = value;
+
+                foreach (var item in _toDoListItems)
+                {
+                    item.DeleteItem += ToDoItemOnDeleteItem;
+                }
+                
+                OnPropertyChanged(nameof(ToDoItems));
+            }
+        }
+
+        public void UpdateListItems(IEnumerable<ToDoListItem> listItems)
+        {
+            ToDoItems = listItems;
+        }
+
+        private async void ToDoItemOnDeleteItem(object? sender, EventArgs e)
+        {
+            await OnDeleteItem((ToDoListItem) sender!);
+        }
+
+        private Task OnDeleteItem(ToDoListItem listItem)
+        {
+            var itemToRemove = ToDoItems.Where(i => i.ItemIdentifier == listItem.ItemIdentifier);
+            ToDoItems = ToDoItems.Except(itemToRemove);
+
+            return Task.CompletedTask;
+        }
     }
 }
