5be10c3158dfeacebc5f19a9e481dfed2f5712f4
Sam Hawkes <40278032+samhawkes@users.noreply.github.com>
2021-05-10 22:18:17 +0100

    Add helpers

---
+++ ToDoApp/Helpers/ObservableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ToDoApp.Helpers
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ToDoApp/Helpers/SimpleCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ToDoApp.Helpers
+{
+    public class SimpleCommand : SimpleCommand<object>
+    {
+        public SimpleCommand(Action<object> action) : base(action)
+        {
+        }
+    }
+
+    public class SimpleCommand<T> : ICommand where T : class
+    {
+        private readonly Action<T> _action;
+
+        protected SimpleCommand(Action<T> action)
+        {
+            _action = action;
+            CanExecuteChanged = (sender, args) => { };
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
